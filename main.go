6a3f1dbb46c8905ce47124630f511208fd1cdecb
Zakaria EL FATHI <70219190+Zakelfathi@users.noreply.github.com>
2023-02-11 19:28:41 +0100

    c

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 func helloWorld(c *fiber.Ctx) error{
-	return c.sendString("HELLO Wrld")
+	return c.SendString("HELLO Wrld")
 }
+
 func main(){
 	app := fiber.New()
 	app.Get("/", helloWorld)
